7261f0b598f7e99ad173600bbe83804310dbeec2
Cody <codybarker.or@gmail.com>
2023-02-02 12:06:13 -0800

    core deliverables achieved

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-// write your code here
+const ramenMenuDiv = document.querySelector('#ramen-menu')
+const detailName = document.querySelector('.name')
+const detailRestaurant = document.querySelector('.restaurant')
+const detailImage = document.querySelector('.detail-image')
+const ratingText = document.querySelector('#rating-display')
+const commentText = document.querySelector('#comment-display')
+const form = document.querySelector('#new-ramen')
+const nameInput = document.querySelector('#new-name')
+const restaurantInput = document.querySelector('#new-restaurant')
+const imageInput = document.querySelector('#new-image')
+const ratingInput = document.querySelector('#new-rating')
+const commentInput = document.querySelector('#new-comment')
+
+document.addEventListener('DOMContentLoaded', renderImages)
+
+function renderImages() {
+    fetch ('http://localhost:3000/ramens')
+    .then((resp) => resp.json())
+    .then(ramenList => {
+        ramenList.forEach(ramen => createImage(ramen))
+    }) 
+}
+
+function createImage(ramen) {
+    const imgTag = document.createElement('img')
+    imgTag.src = ramen.image
+    ramenMenuDiv.append(imgTag)
+    const name = ramen.name
+    const restaurant = ramen.restaurant
+    const image = ramen.image
+    const rating = ramen.rating
+    const comment = ramen.comment
+    imgTag.addEventListener('click', () => {
+        detailName.innerText = name
+        detailRestaurant.innerText = restaurant
+        detailImage.src = image
+        ratingText.innerText = rating
+        commentText.innerText = comment
+    })
+}
+
+form.addEventListener('submit', addRamen)
+
+function addRamen (e) {
+    e.preventDefault()
+    const newRamen = {
+      name: nameInput.value,
+      restaurant: restaurantInput.value,
+      image: imageInput.value,
+      rating: ratingInput.value,
+      comment: commentInput.value,
+    }
+    createImage(newRamen)
+}    
